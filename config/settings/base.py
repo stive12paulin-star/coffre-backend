5e84e3dff89ee5ca12699cdc15abf8c3d506f513
Stive Paulin <stive12paulin@gmail.com>
2026-09-01 23:23:13 +0000

    ajout CORS_ALLOWED_ORIGIN_REGEXES pour URLs preview Vercel

--- config/settings/base.py
+++ config/settings/base.py
@@ -112,3 +112,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://coffre-frontend.vercel.app",
 ]
+CORS_ALLOWED_ORIGIN_REGEXES = [
+r"^https://coffre-frontend.*\.vercel\.app$",
+]
